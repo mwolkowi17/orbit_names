098a5b0b651f9364adc382fb79a18b2697155a3d
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-12-09 22:32:13 +0100

    transform controll

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Scene } from './Loader';
-import { Html, OrbitControls } from '@react-three/drei';
+import { Html, OrbitControls, PresentationControls } from '@react-three/drei';
 
 
 
@@ -17,20 +17,20 @@ function App() {
     return <Html center style={{ color: 'white' }}>loading...</Html>
   }
 
-  
+
   return (
     <div>
       <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 8] }}>
         <Suspense fallback={null}>
-          <OrbitControls />
-          <ambientLight />
-          <pointLight position={[10, 20, 10]} />
-          <pointLight position={[-5, -15, 30]} />
-          <Suspense fallback={<Loader />}>
-            <Scene pos={[4,3,0]} rot={[Math.PI*0.2,0,0]}/>
-          </Suspense>
+          <PresentationControls snap={true}  polar={[0, Math.PI / 2]} >
+            <ambientLight />
+            <pointLight position={[10, 20, 10]} />
+            <pointLight position={[-5, -15, 30]} />
+            <Suspense fallback={<Loader />}>
+              <Scene pos={[4, 3, 0]} rot={[Math.PI * 0.2, 0, 0]} />
+            </Suspense>
 
-
+          </PresentationControls>
         </Suspense>
       </Canvas>
     </div>
@@ -40,4 +40,3 @@ function App() {
 
 export default App;
 
-    
