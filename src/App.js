acb92618b2e58a046a017fc227cfb92579776bcc
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-12-10 09:24:01 +0100

    new model

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Scene } from './Loader';
-import { Html, OrbitControls, PresentationControls } from '@react-three/drei';
+import { Html, OrbitControls } from '@react-three/drei';
 
 
 
@@ -22,7 +22,8 @@ function App() {
     <div>
       <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 8] }}>
         <Suspense fallback={null}>
-          <PresentationControls snap={true}  polar={[0, Math.PI / 2]} >
+          {/*<PresentationControls snap={true}  polar={[0, Math.PI / 2]} >**/}
+          <OrbitControls/>
             <ambientLight />
             <pointLight position={[10, 20, 10]} />
             <pointLight position={[-5, -15, 30]} />
@@ -30,7 +31,7 @@ function App() {
               <Scene pos={[4, 3, 0]} rot={[Math.PI * 0.2, 0, 0]} />
             </Suspense>
 
-          </PresentationControls>
+          {/*</PresentationControls>*/}
         </Suspense>
       </Canvas>
     </div>
